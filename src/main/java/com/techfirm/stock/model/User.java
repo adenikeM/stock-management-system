f4b57d761b3467ca7aa11fdae3a81a5221849a86
Moronkola Adijat <horythorkeh@gmail.com>
2024-02-23 14:53:34 +0100

    update application properties address entity gender entity update user entity

--- src/main/java/com/techfirm/stock/model/User.java
+++ src/main/java/com/techfirm/stock/model/User.java
@@ -1,28 +1,51 @@
 package com.techfirm.stock.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.techfirm.stock.model.enumeration.Gender;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Data
+@Table(name = "user_profile")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
+
+    @NotEmpty(message = "Last name cannot be null")
+    @Column(length = 20, nullable = false)
     private String lastName;
+
+    @NotNull
+    @Column(length = 20, nullable = false)
     private String firstName;
-    private String otherName;
-    private Enum gender;
-    private Date birthDate;
+
+    private String otherNames;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Past
+    private LocalDate birthDate;
+
+    @NotEmpty
+    @Column(name="user_name", nullable = false, unique = true, updatable = false)
     private String userName;
-    private String address;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @OneToOne
+    private Address address;
+
+    //todo add annotation for password validator
     private String password;
-
-
 }
