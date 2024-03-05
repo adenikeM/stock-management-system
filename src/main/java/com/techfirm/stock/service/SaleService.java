ccf70123a3906bd860170458f1fcc3e22bf29757
Moronkola Adijat <horythorkeh@gmail.com>
2024-03-05 12:14:27 +0100

    sale service

---
+++ src/main/java/com/techfirm/stock/service/SaleService.java
@@ -0,0 +1,47 @@
+package com.techfirm.stock.service;
+
+import com.techfirm.stock.model.Product;
+import com.techfirm.stock.model.Role;
+import com.techfirm.stock.model.Sale;
+import com.techfirm.stock.model.User;
+import com.techfirm.stock.repository.ProductRepository;
+import com.techfirm.stock.repository.SaleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SaleService {
+    private final SaleRepository saleRepository;
+    private final ProductRepository productRepository;
+
+    public SaleService(SaleRepository saleRepository, ProductRepository productRepository) {
+        this.saleRepository = saleRepository;
+        this.productRepository = productRepository;
+    }
+    public List<Sale> getAllSale(){
+        return saleRepository.findAll();
+    }
+    public Optional<Sale> getSale(Integer id){ return saleRepository.findById(id);}
+    public Sale createSale(Sale sale){
+        List<Product> productList = savedProductsWithRepo(sale.getProducts());
+        sale.setProducts(productList);
+        return saleRepository.save(sale);
+    }
+    private List<Product> savedProductsWithRepo(List<Product> products) {
+        return productRepository.saveAll(products);
+    }
+    public Optional<Sale> UpdateSale(Sale sale){
+        saleRepository.findById(sale.getId());
+        if(sale.getId() == null){
+            throw  new IllegalArgumentException("Sale id cannot be null");
+        }
+        List<Product> productList = savedProductsWithRepo(sale.getProducts());
+        sale.setProducts(productList);
+        return Optional.of(saleRepository.save(sale));
+    }
+    public void deleteSale(Integer id){
+        saleRepository.findById(id);
+    }
+}
