47eb2fec810f56f0c1223270499cc521c42bdb3d
Moronkola Adijat <horythorkeh@gmail.com>
2024-03-07 22:07:57 +0100

    role service

---
+++ src/main/java/com/techfirm/stock/service/RoleService.java
@@ -0,0 +1,37 @@
+package com.techfirm.stock.service;
+
+import com.techfirm.stock.model.Location;
+import com.techfirm.stock.model.Role;
+import com.techfirm.stock.repository.LocationRepository;
+import com.techfirm.stock.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+    public List<Role> getAllRole(){
+        return roleRepository.findAll();
+    }
+    public Optional<Role> getRole(Integer id){
+        return roleRepository.findById(id);
+    }
+    public Role createRole(Role role){
+        return roleRepository.save(role);
+    }
+    public Optional<Role> updateRole(Role role){
+       roleRepository.findById(role.getId());
+        if(role.getId() == null){
+            throw  new IllegalArgumentException("Role id must not be null");
+        }
+        return Optional.of(roleRepository.save(role));
+    }
+    public void deleteRole(Integer id){roleRepository.findById(id);
+    }
+}
