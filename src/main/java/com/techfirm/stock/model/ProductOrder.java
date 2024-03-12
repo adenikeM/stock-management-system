b1cb60f181565d4807d138f145c4a3480c6635d5
Moronkola Adijat <horythorkeh@gmail.com>
2024-03-12 12:56:20 +0100

    product order entity

---
+++ src/main/java/com/techfirm/stock/model/ProductOrder.java
@@ -0,0 +1,44 @@
+package com.techfirm.stock.model;
+
+import com.techfirm.stock.model.enumeration.OrderStatus;
+import com.techfirm.stock.model.enumeration.QualityCheck;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ProductOrder implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productOrderId;
+
+    @Column(length = 20, nullable = false)
+    private Double quantityOrder;
+
+    @Column(length = 20, nullable = false)
+    private Double pricePerUnit;
+
+    @Enumerated(EnumType.STRING)
+    private QualityCheck qualityCheck;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @Past
+    private LocalDate orderedDate;
+
+    @Past
+    private LocalDate deliveryDate;
+
+    @ManyToOne
+    private Product product;
+
+
+
+}
