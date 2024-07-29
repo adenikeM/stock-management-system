e31796ed456c5b33e8c9b742bd18eb3fe6912282
Moronkola Adijat <horythorkeh@gmail.com>
2024-07-29 09:20:49 +0100

    updating analyticsOverview service

--- src/main/java/com/techfirm/stock/service/AnalyticsOverviewService.java
+++ src/main/java/com/techfirm/stock/service/AnalyticsOverviewService.java
@@ -1,19 +1,15 @@
 package com.techfirm.stock.service;
 
 import com.techfirm.stock.model.AnalyticsOverview;
-import com.techfirm.stock.model.ProductCategory;
-import com.techfirm.stock.model.dto.ProductCategoryCount;
 import com.techfirm.stock.repository.ProductCategoryRepository;
 import com.techfirm.stock.repository.ProductRepository;
 import com.techfirm.stock.repository.ReturnedSalesRepository;
 import com.techfirm.stock.repository.SalesRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 @Service
 public class AnalyticsOverviewService {
-    private  final ProductRepository productRepository;
+    private final ProductRepository productRepository;
     private final SalesRepository salesRepository;
     private final ReturnedSalesRepository returnedSalesRepository;
     private final ProductCategoryRepository productCategoryRepository;
@@ -25,20 +21,13 @@ public class AnalyticsOverviewService {
         this.productCategoryRepository = productCategoryRepository;
     }
 
-    public AnalyticsOverview getAnalyticsOverview(){
-        AnalyticsOverview overview = new AnalyticsOverview();
-
-        Long productCount = productRepository.count();
-        overview.setProductCount(productCount);
-
-        Long salesCount = salesRepository.count();
-        overview.setSalesCount(salesCount);
-
-        Long returnedSalesCount = returnedSalesRepository.count();
-        overview.setReturnedSalesCount(returnedSalesCount);
-
-        Long productCategoryCount = productCategoryRepository.count();
-        overview.setProductCategoryCount(productCategoryCount);
+    public AnalyticsOverview getAnalyticsOverview() {
+        return AnalyticsOverview.builder()
+                                .productCount(productRepository.count())
+                                .salesCount(salesRepository.count())
+                                .returnedSalesCount(returnedSalesRepository.count())
+                                .productCategoryCount(productCategoryRepository.count())
+                                .build();
 
 //        List<ProductCategory> categories = productCategoryRepository.findAll();
 //        List<ProductCategoryCount> categoryCounts = new ArrayList<>();
@@ -48,11 +37,8 @@ public class AnalyticsOverviewService {
 //            categoryCounts.add(categoryCount);
 //        }
 //        overview.setCategoryCounts(categoryCounts);
-
-        return overview;
     }
-
-    }
+}
 
 
 
